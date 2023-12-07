bee7545c6ee9e504dd89f38bb98f1391cbaf2001
Pushpendra Babu <code.pushpendra@gmail.com>
2023-12-07 19:12:39 +0530

    first commit

---
+++ src/main/java/com/render/springrender/SpringRenderApplication.java
@@ -0,0 +1,13 @@
+package com.render.springrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/render/springrender/SpringRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.render.springrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
